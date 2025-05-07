bb73c380a0bf5b341f886195a0c61b1b4a83b0ab
WCLynn <87233065+WCLynn@users.noreply.github.com>
2025-05-07 10:52:25 +0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# 你的 Channel Access Token 和 Channel Secret
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("SECRET")
+app = Flask(__name__)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # 取得 LINE 的簽章 (header)
+    signature = request.headers['X-Line-Signature']
+
+    # 取得請求的 body 內容
+    body = request.get_data(as_text=True)
+
+    try:
+        # 驗證簽章並處理訊息
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# 處理文字訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=f"你說的是：{user_message}")
+    )
+
+if __name__ == "__main__":
+    app.run(port=5000)
